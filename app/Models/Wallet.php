3fa62d99657a4b88142a101321be80a92db0180f
DylanLangevin <dylan.lgvn@gmail.com>
2025-03-19 16:11:08 +0100

    wip: notif user when balance is low

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -8,15 +8,27 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\User;
+use App\Notifications\BalanceLow;
 
 class Wallet extends Model
 {
     use HasFactory;
 
+    public const NOTIFIABLE_LIMIT = 10;
+
     protected $fillable = [
         'user_id',
         'balance'
     ];
+
+    protected static function booted()
+    {
+        static::updated(function (Wallet $wallet) {
+
+            $wallet->checkBalance();
+        });
+    }
 
     /**
      * @return BelongsTo<User>
@@ -32,5 +44,24 @@ class Wallet extends Model
     public function transactions(): HasMany
     {
         return $this->hasMany(WalletTransaction::class);
+    }
+
+    // /**
+    //  * @return 
+    //  */
+    public function checkBalance()
+    {
+        if (!$this || !$this->balance) {
+            return;
+        }
+
+        if (($this->balance/100) < self::NOTIFIABLE_LIMIT) {
+
+            $user = $this->user; 
+
+            if ($user) {
+                $user->notify(new BalanceLow());
+            }
+        }
     }
 }
